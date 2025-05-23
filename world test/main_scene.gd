da88f8df3980c72d4008a8491488fcc19f378876
evanlu1134 <evanlu1134@gmail.com>
2025-05-23 18:10:34 -0400

    added basic naming pass through scenes logic

--- world test/main_scene.gd
+++ world test/main_scene.gd
@@ -1,20 +1,24 @@
 extends Control
 
+@onready var player_one_input = $VBoxContainer/Player1Input
+@onready var player_two_input = $VBoxContainer/Player2Input
+@onready var start = $start
+	
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	AudioController.intro_song()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_start_pressed() -> void:
-	AudioController.stop_all_music(get_tree().current_scene)
-	get_tree().change_scene_to_file("res://world test/world.tscn")
-
-
+	var name1 = player_one_input.text.strip_edges()
+	var name2 = player_two_input.text.strip_edges()
+	if name1 == "" or name2 == "":
+		print("Both players must enter their names.")
+	else:
+	#next scene flow
+		PlayerInfo.player1_name = name1
+		PlayerInfo.player2_name = name2
+		AudioController.stop_all_music(get_tree().root)
+		get_tree().change_scene_to_file("res://world test/world.tscn")
+		
 func _on_htp_pressed() -> void:
-	print("htp")
+	print("opt")
