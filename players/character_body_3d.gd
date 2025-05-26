4f443aa4f970504326e4c650ea3512ec7dcbc7ca
AGarciaNY <aldairgarcia360@gmail.com>
2025-05-26 00:43:55 -0400

    0.0.7.1 fix sword bug

--- players/character_body_3d.gd
+++ players/character_body_3d.gd
@@ -200,12 +200,12 @@ func _on_hit_box_area_entered(area: Area3D) -> void:
 	if area.is_in_group("fist") and area.is_in_group(enemy_fist) and area.current_status == "punching":
 		damge(4, 0.1)
 	elif area.is_in_group("sword") and area.is_in_group(enemy_fist) and area.get_status() == "swing":
-		damge(16,0.4)
+		damge(6,0.15)
 	elif area.is_in_group("ammo_box") and current_tool == "gun" and set_tool.get_data() <6:
 		area.remove_box()
 		set_tool.reload_gun()
 	elif area.is_in_group("bullet") and area.is_in_group(enemy_fist):
-		damge(20, 0.5)
+		damge(12, 0.3)
 	elif area.is_in_group("env_damage"):
 		var env_damage_amount = area.damage
 		damge(env_damage_amount, 0.5)
--- tools/sword/damage_box.gd
+++ tools/sword/damage_box.gd
@@ -4,4 +4,4 @@ func damge():
 	return {damge:16,scale:.4}
 
 func get_status():
-	return $"../..".current_status
+	return $"../../..".current_status
